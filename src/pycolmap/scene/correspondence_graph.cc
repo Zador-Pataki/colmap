9e1954a8d6036469ea129f372172e6da80f4c6bd
Zador-Pataki <zadorpataki14@gmail.com>
2026-04-28 16:42:04 +0200

    pycolmap: bind LC features (RA, GP, track establishment, scene types)
    Scene: Track.lc_elements, Image.is_registered, ImagePair.is_LC/are_lc.
    RA: skip_risky_LC_pairs, use_video_constraints, video loss scales;
    run_rotation_averaging gains correspondence_graph + extract_final_weights.
    GP: use_lc_observations, loss_lc_geometry, loss_lc_depth.
    Track establishment: establish_full_tracks (lc_second_pass), find_tracks_for_problem.
    GlobalMapperOptions: track_lc_second_pass, track_max_num_views/tracks, track_two_view_depth_gate.

--- src/pycolmap/scene/correspondence_graph.cc
+++ src/pycolmap/scene/correspondence_graph.cc
@@ -55,6 +55,7 @@ void BindCorrespondenceGraph(py::module& m) {
       .def_readwrite("weight", &CorrespondenceGraph::ImagePair::weight)
       .def_readwrite("matches", &CorrespondenceGraph::ImagePair::matches)
       .def_readwrite("inliers", &CorrespondenceGraph::ImagePair::inliers)
+      .def_readwrite("is_LC", &CorrespondenceGraph::ImagePair::is_LC)
       .def_readwrite("cov_t", &CorrespondenceGraph::ImagePair::cov_t)
       .def_readwrite("are_lc", &CorrespondenceGraph::ImagePair::are_lc)
       .def_readwrite("num_matches",
